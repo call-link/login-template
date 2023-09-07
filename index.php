f3ccae3c9bc659d08cbeb68433fdaaeed78477bd
Hala company <hadii.mohammadii1379@gmail.com>
2023-09-07 03:19:50 -0700

    add project to ripository

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+require 'tpl/register_tpl.php' ;
---
+++ tpl/register_tpl.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register page</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+ 
+</head>
+<body direction="ltr">
+    <!--start section from  -->
+    <section class="contener">
+        <form action="" method="" class="login" id="form" enctype="multipart/form-data">
+            <h1 class="h-1">Register</h1>
+            <input type="text" class="name" name="name" placeholder="نام کاربری..." autofocus>
+            <input type="email" class="email" name="email" placeholder="ایمیل...">
+            <input type="tel" class="phone" name="phone"  placeholder="شماره تلفن همراه..." maxlength="11">
+            <input type="password" class="password" name="pass" placeholder="رمز عبور..." minlength="6">
+            <input type="text" class="bio" name="bio" placeholder="در باره من...">
+            <input type="file" name="upload_image" class="custom-image-upload">
+            <input type="submit" class="submit submitHover" name="sub" value="ثبت نام">
+        </form>
+    </section>
+    <!--end section from  -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <footer class="footer">
+        <p>create by hala team | programmer(Github) : <a href="https://github.com/codehod">codehod</a> | telegram id :  @hadi_mohammadi1379 </p>
+    </footer>
+</body>
+</html> 
